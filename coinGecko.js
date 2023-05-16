f6db0a40e7d5d21360040337f3fdc820a8d09470
Kartik Punia <kpunia@berkeley.edu>
2023-05-16 01:17:32 -0700

    Pushed many changes: UI Updates, updated select menu, and integrating manual input

--- coinGecko.js
+++ coinGecko.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     // make HTTP requests to API endpoint to get crypto prices
     calculateButton.addEventListener("click", function() {
-        
+
         // retrieve selected cryptos from dropdown menus
         const selectCrypto1 = document.querySelector('[name=crypto1]');
         const selectCrypto2 = document.querySelector('[name=crypto2]')
@@ -32,10 +32,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
         .then(data => {
             // extract cryptocurrency prices from response data
             var numTokens1 = data[0][0].max_supply;
-            const fdv2 = data[1][0].fully_diluted_valuation;
+            var fdv2 = data[1][0].fully_diluted_valuation;
             if (numTokens1 == null) {
-                numTokens1 = data[0][0].fully_diluted_valuation / data[0][0].current_price
-            }// eth, sol, dot, trx, ton
+                if (data[0][0].fully_diluted_valuation == null) {
+                    numTokens1 = data[0][0].circulating_supply
+                }
+                else {
+                    numTokens1 = data[0][0].fully_diluted_valuation / data[0][0].current_price
+                }
+            }// eth, sol, dot, trx, ton || doge, shiba, leo
+            if (fdv2 == null) {
+                fdv2 = data[1][0].circulating_supply * data[1][0].current_price
+            }// doge, shiba, leo
+            
             // calculate and display the FDV
             const fdv1 = (fdv2 / numTokens1).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
             resultHead.textContent = `The price of ${crypto1Name} with the FDV of ${crypto2Name}:`
@@ -43,4 +52,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         })
         .catch(error => console.error('Error:', error));
     })
+
+    // Adding manual cryptocurrency
+    document.getElementById('add').addEventListener('click', function() {
+        var name = document.getElementById('name').value;
+        var valuation = "$" + document.getElementById('valuation').value;
+    
+        // You may want to do some validation on newCryptoName and newCryptoValue here
+    
+        var selectizeA = $('select[name=crypto1]')[0].selectize;
+        var selectizeB = $('select[name=crypto2]')[0].selectize;
+    
+        selectizeA.addOption({value: valuation, text: name});
+        selectizeB.addOption({value: valuation, text: name});
+
+        document.getElementById('manual').style.display = 'none';
+    });
 })
