cf3ebf8a0860ace2f723054ddf975af63d4d65de
Kartik Punia <kpunia@berkeley.edu>
2023-05-16 15:19:08 -0700

    Added manual, adding currency-selector

--- coinGecko.js
+++ coinGecko.js
@@ -20,37 +20,77 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const resultHead = document.getElementById('result_head');
         const resultElement = document.getElementById('result');
         
-        // make HTTP requests to API endpoint to get crypto prices
-        const promises = [
-            fetch(`${apiEndpoint}?vs_currency=usd&ids=${crypto1}&sparkline=false`),
+        if (crypto1.substring(0, 1) == "$" && crypto2.substring(0, 1) == "$") {
+            resultHead.textContent = `A and B cannot both be manual entries`
+            resultElement.textContent = ""
+        } else if (crypto1.substring(0, 1) == "$") {
             fetch(`${apiEndpoint}?vs_currency=usd&ids=${crypto2}&sparkline=false`)
-        ];
+            .then(response => response.json())
+            .then(data => {
+                const valuation1 = +crypto1.substring(1)
+                const price2 = data[0].current_price
+                var fdv2 = data[0].fully_diluted_valuation
+                if (fdv2 == null) {
+                    fdv2 = data[0].circulating_supply * data[0].current_price
+                }
 
-        // confusing
-        Promise.all(promises)
-        .then(responses => Promise.all(responses.map(r => r.json())))
-        .then(data => {
-            // extract cryptocurrency prices from response data
-            var numTokens1 = data[0][0].max_supply;
-            var fdv2 = data[1][0].fully_diluted_valuation;
-            if (numTokens1 == null) {
-                if (data[0][0].fully_diluted_valuation == null) {
-                    numTokens1 = data[0][0].circulating_supply
+                const price1 = (price2 / fdv2 * valuation1).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+                resultHead.textContent = `${crypto1Name} with the FDV of ${crypto2Name}:`
+                resultElement.textContent = price1;
+            })
+            .catch(error => console.error('Error:', error));
+        } else if (crypto2.substring(0, 1) == "$") {
+            fetch(`${apiEndpoint}?vs_currency=usd&ids=${crypto1}&sparkline=false`)
+            .then(response => response.json())
+            .then(data => {
+                var numTokens1 = data[0].max_supply
+                const valuation2 = +crypto2.substring(1)
+                if (numTokens1 == null) {
+                    if (data[0].fully_diluted_valuation == null) {
+                        numTokens1 = data[0].circulating_supply
+                    }
+                    else {
+                        numTokens1 = data[0].fully_diluted_valuation / data[0].current_price
+                    }
                 }
-                else {
-                    numTokens1 = data[0][0].fully_diluted_valuation / data[0][0].current_price
-                }
-            }// eth, sol, dot, trx, ton || doge, shiba, leo
-            if (fdv2 == null) {
-                fdv2 = data[1][0].circulating_supply * data[1][0].current_price
-            }// doge, shiba, leo
-            
-            // calculate and display the FDV
-            const fdv1 = (fdv2 / numTokens1).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-            resultHead.textContent = `The price of ${crypto1Name} with the FDV of ${crypto2Name}:`
-            resultElement.textContent = fdv1;
-        })
-        .catch(error => console.error('Error:', error));
+                const price1 = (valuation2 / numTokens1).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+                resultHead.textContent = `${crypto1Name} with the private valuation of ${crypto2Name}:`
+                resultElement.textContent = price1;
+            })
+            .catch(error => console.error('Error:', error));
+        } else {
+            // make HTTP requests to API endpoint to get crypto prices
+            const promises = [
+                fetch(`${apiEndpoint}?vs_currency=usd&ids=${crypto1}&sparkline=false`),
+                fetch(`${apiEndpoint}?vs_currency=usd&ids=${crypto2}&sparkline=false`)
+            ];
+
+            // confusing
+            Promise.all(promises)
+            .then(responses => Promise.all(responses.map(r => r.json())))
+            .then(data => {
+                // extract cryptocurrency prices from response data
+                var numTokens1 = data[0][0].max_supply;
+                var fdv2 = data[1][0].fully_diluted_valuation;
+                if (numTokens1 == null) {
+                    if (data[0][0].fully_diluted_valuation == null) {
+                        numTokens1 = data[0][0].circulating_supply
+                    }
+                    else {
+                        numTokens1 = data[0][0].fully_diluted_valuation / data[0][0].current_price
+                    }
+                }// eth, sol, dot, trx, ton || doge, shiba, leo
+                if (fdv2 == null) {
+                    fdv2 = data[1][0].circulating_supply * data[1][0].current_price
+                }// doge, shiba, leo
+                
+                // calculate and display the FDV
+                const price1 = (fdv2 / numTokens1).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+                resultHead.textContent = `${crypto1Name} with the FDV of ${crypto2Name}:`
+                resultElement.textContent = price1;
+            })
+            .catch(error => console.error('Error:', error));
+        }
     })
 
     // Adding manual cryptocurrency
