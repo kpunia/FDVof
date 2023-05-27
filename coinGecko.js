ad05f303cb48fe8f0e6e1af06abee4d9534e1152
Kartik Punia <kpunia@berkeley.edu>
2023-05-27 11:08:17 -0700

    Added total supply to manual

--- coinGecko.js
+++ coinGecko.js
@@ -33,15 +33,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
             .then(response => response.json())
             .then(data => {
                 const valuation1 = +crypto1.substring(1)
+                const selectizeA = $('select[name=crypto1]')[0].selectize;
+                const selectedValue = selectizeA.getValue();
+                const selectedOption = selectizeA.options[selectedValue];
+                const supply1 = selectedOption.supply;
                 const price2 = data[0].current_price
                 var fdv2 = data[0].fully_diluted_valuation
                 if (fdv2 == null) {
                     fdv2 = data[0].circulating_supply * data[0].current_price
                 }
-
+                console.log(supply1);
                 const price1 = (price2 / fdv2 * valuation1).toLocaleString('en-US', { style: 'currency', currency: currency });
                 resultHead.textContent = `${crypto1Name} with the FDV of ${crypto2Name}:`
-                resultElement.textContent = price1;
+                if (supply1 != 0) {
+                    const priceRatio = ((price2 / fdv2 * valuation1)/(valuation1 / supply1)).toFixed(2);
+                    if (priceRatio >= 1) {
+                        resultElement.innerHTML = `${price1} <span style="color: lime;">(${priceRatio}x)</span>`;
+                    } else {
+                        resultElement.innerHTML = `${price1} <span style="color: red;">(${priceRatio}x)</span>`;
+                    }
+                } else {
+                    resultElement.textContent = price1;
+                }
             })
             .catch(error => {
                 alert('CoinGecko is processing too many calls right now. Please try again later');
@@ -123,10 +136,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('add').addEventListener('click', function() {
         var name = document.getElementById('name').value;
         var valuation = document.getElementById('valuation').value;
+        var supply = document.getElementById('supply').value;
     
         // Validation for Private Valuation
         if (isNaN(valuation)) {
             alert("Please enter a number for Private Valuation");
+            return;
+        }
+
+        if (isNaN(supply)) {
+            alert("Please enter a number for Total Supply");
             return;
         }
 
@@ -135,11 +154,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         var selectizeA = $('select[name=crypto1]')[0].selectize;
         var selectizeB = $('select[name=crypto2]')[0].selectize;
     
-        selectizeA.addOption({value: valuation, text: name});
-        selectizeB.addOption({value: valuation, text: name});
+        selectizeA.addOption({value: valuation, text: name, supply: supply});
+        selectizeB.addOption({value: valuation, text: name, supply: supply});
 
         document.getElementById('name').value = '';
         document.getElementById('valuation').value = '';
+        document.getElementById('supply').value = '';
         document.getElementById('manual').style.display = 'none';
     });
 })
